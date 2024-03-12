7b0b1ac3e28328f57cf3e97311d897ad3d007ff7
Volodymyr Gerun <vgerun97@gmail.com>
2024-03-12 16:58:58 +0200

    Add PWA approach

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
@@ -7,9 +7,25 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  applicationName: "Diet ME",
   title: "Diet Made Easy: Track Calories with Just a Photo!",
   description:
     "Snap a picture of your food, instantly get calorie info. No counting, no stress.",
+  manifest: "/manifest.json",
+  icons: {
+    icon: "/icons/favicon.ico",
+  },
+  keywords: "diet, calorie, food, track, photo",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "black-translucent",
+    title: "Diet Made Easy",
+    startupImage: "/icons/apple-touch-icon.png",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
