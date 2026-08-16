e52805db0f201df8b7542032cbe52d2539a1d857
uigywnkiub <vgerun97@gmail.com>
2026-08-16 14:04:20 +0300

    style: arrange header on mobile

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
       <h1 className='mb-6 text-center text-2xl font-extrabold drop-shadow-[0_1px_1px_rgba(0,0,0,0.15)] md:mb-8 md:text-3xl dark:drop-shadow-[0_1px_1px_rgba(0,0,0,0.3)]'>
         <span className='bg-gradient-to-r from-red-400 to-blue-600 bg-clip-text text-transparent'>
           D
-          <span className='-ml-2 -mr-2.5 inline-block -translate-y-2 rotate-[320deg] text-2xl text-[initial] [-webkit-text-fill-color:initial] md:text-3xl'>
+          <span className='-ml-1.5 -mr-2 inline-block -translate-y-1.5 rotate-[320deg] text-2xl text-[initial] [-webkit-text-fill-color:initial] md:-ml-2 md:-mr-2.5 md:-translate-y-2 md:text-3xl'>
             🥕
           </span>
           et Made Easy
